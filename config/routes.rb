caa9fb75684d77f686049c1a0ba4e4f0cde62556
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-20 17:09:49 -0600

    Add route to ItemsController#index
    Co-Authored-By: Stella Mainar <smainar@users.noreply.github.com>
    Co-Authored-By: null <hsmitha26@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "welcome#index"
+
+  resources :items, only: [:index]
+
+  get '/login', to: "sessions#new"
 end
