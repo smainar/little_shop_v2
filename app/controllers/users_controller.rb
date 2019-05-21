82b7889bc4e356f12fdbabb2100c9eca8c676574
billy urrutia <billyurrutia@gmail.com>
2019-05-21 12:51:21 -0600

    Created show,create,user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
+  end
+
+
+  def show
+
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/profile'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:address,:city,:state,:zip,:password,:password_confirmation)
   end
 end
