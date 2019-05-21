81476b405a592700b1612bf8ba58e28c0d4622f5
Stella Mainar <43945779+smainar@users.noreply.github.com>
2019-05-21 15:17:07 -0600

    Delete parens on line 15 & 16
    - Delete parentheses around :email (line 15) and :password (line 16) for uniformity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,13 +12,12 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :state}
     it {should validate_presence_of :zip}
 
-    it {should validate_uniqueness_of(:email)}
-    it {should validate_presence_of(:password)}
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :password}
   end
 
   describe 'relationships' do
     it {should have_many :items}
     it {should have_many :orders}
   end
-
 end
