afaf1a18f01915a04f927f869f067ecd03802139
billy urrutia <billyurrutia@gmail.com>
2019-05-21 14:13:33 -0600

    added flash[:error] message and welcome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,16 @@ class UsersController < ApplicationController
   def show
 
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      flash[:welcome] = "Welcome, #{@user.name}"
       redirect_to '/profile'
     else
-      render :new
+      flash[:error] = "Failed to create account"
+      redirect_to '/register'
     end
   end
 
