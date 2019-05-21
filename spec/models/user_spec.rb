ea6499f14f38b5b5397ab50856b51fa5bb1ad53f
billy urrutia <billyurrutia@gmail.com>
2019-05-21 09:08:38 -0600

    added test for validating uniqueness of name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'validates' do
+  describe 'validations' do
     it {should validate_presence_of :email}
-    it {should validate_presence_of :password_digest}
     # it {should validate_presence_of :role}
     it {should validate_presence_of :active}
     it {should validate_presence_of :name}
@@ -12,6 +11,9 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :city}
     it {should validate_presence_of :state}
     it {should validate_presence_of :zip}
+
+    it {should validate_uniqueness_of(:name)}
+    it {should validate_presence_of(:password)}
   end
 
   describe 'relationships' do
