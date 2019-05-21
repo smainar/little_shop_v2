6c512cd59f403c1e9e6839eceac9add69c8871c9
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-21 15:12:35 -0600

    completes user story 14

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   validates_presence_of :name,
-                       :active,
                        :price,
                        :description,
                        :image,
@@ -9,4 +8,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+
+  def self.active_items
+    self.where(active: true)
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
   validates_presence_of :price_per_item
   validates_presence_of :quantity
 end
