cdc144331d4375111d27a2460d7740a8ce5e919a
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-22 17:39:51 -0600

    Updated Test After Comments!
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- spec/features/users/user_logout_spec.rb
+++ spec/features/users/user_logout_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "User logout, " do
-  before :each do
-    @user = create(:user)
+
+  it "can log out A USER who is logged in" do
+    user = create(:user)
 
     visit login_path
 
     within(".login-form") do
-      fill_in "Email", with: @user.email
-      fill_in "Password", with: @user.password
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
       click_on "Login"
     end
-  end
-  it "can log out any user who is logged in" do
 
     expect(current_path).to eq(profile_path)
 
@@ -25,9 +24,62 @@ RSpec.describe "User logout, " do
     expect(page).to_not have_link("Log Out")
   end
 
+  it "can log out AN ADMIN who is logged in" do
+    admin = create(:admin)
+
+    visit login_path
+
+    within(".login-form") do
+      fill_in "Email", with: admin.email
+      fill_in "Password", with: admin.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(root_path)
+
+    click_link "Log Out"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You are logged out!")
+    expect(page).to have_link("Login")
+    expect(page).to have_link("Register")
+    expect(page).to_not have_link("Log Out")
+  end
+
+  it "can log out A MERCHANT who is logged in" do
+    merchant = create(:merchant)
+
+    visit login_path
+
+    within(".login-form") do
+      fill_in "Email", with: merchant.email
+      fill_in "Password", with: merchant.password
+      click_on "Login"
+    end
+
+    expect(current_path).to eq(dashboard_path)
+
+    click_link "Log Out"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("You are logged out!")
+    expect(page).to have_link("Login")
+    expect(page).to have_link("Register")
+    expect(page).to_not have_link("Log Out")
+  end
+
   it 'empties cart on logout' do
     merchant = create(:merchant)
+    user = create(:user)
     item_1 = create(:item, user: merchant)
+
+    visit login_path
+
+    within(".login-form") do
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Login"
+    end
 
     visit item_path(item_1)
 
@@ -40,6 +92,5 @@ RSpec.describe "User logout, " do
 
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Cart: 0")
-
   end
 end
