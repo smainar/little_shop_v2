98470967e59d4fce51387d3ad048022160535512
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-22 16:10:53 -0600

    Add ability for visitor to add items to cart
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>
    Co-Authored-By: Michael Karnes <mikekarnes123@users.noreply.github.com>
    Co-Authored-By: Billy Urrutia <CosmicSpagetti@users.noreply.github.com>
    Co-Authored-By: null <hsmitha26@users.noreply.github.com>

--- spec/features/carts/new_spec.rb
+++ spec/features/carts/new_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "When a visitor adds items to their cart" do
   before(:each) do
     @merchant_1 = create(:merchant)
-    @item_1 = create(:item, user: @merchant_1)
-    @item_2 = create(:item, user: @merchant_1)
+    @item_1 = create(:item, user: @merchant_1, name: "Sofa")
+    @item_2 = create(:item, user: @merchant_1, name: "Chair")
   end
 
   it "displays a message" do
     visit item_path(@item_1)
     click_button "Add to Cart"
 
-    expect(page).to have_content("You now have 1 of #{@item_1.name} in your cart.")
+    expect(page).to have_content("You now have 1 #{@item_1.name} in your cart.")
   end
 
   it "the message correctly increments for multiple items" do
@@ -24,7 +24,7 @@ RSpec.describe "When a visitor adds items to their cart" do
     visit item_path(@item_1)
     click_button "Add to Cart"
 
-    expect(page).to have_content("You now have 2 of #{@item_1.name} in your cart.")
+    expect(page).to have_content("You now have 2 #{@item_1.name}s in your cart.")
   end
 
   it "displays the total number of items in the cart" do
