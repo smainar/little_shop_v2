6a85ec3b3121be74bbf59958f94650c1b23085b5
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-22 16:17:22 -0600

    Add feature test and view for signed-in user adding item to cart
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>
    Co-Authored-By: Michael Karnes <mikekarnes123@users.noreply.github.com>
    Co-Authored-By: Billy Urrutia <CosmicSpagetti@users.noreply.github.com>
    Co-Authored-By: null <hsmitha26@users.noreply.github.com>

--- spec/features/carts/new_spec.rb
+++ spec/features/carts/new_spec.rb
@@ -1,49 +1,105 @@
 require 'rails_helper'
 
-RSpec.describe "When a visitor adds items to their cart" do
-  before(:each) do
-    @merchant_1 = create(:merchant)
-    @item_1 = create(:item, user: @merchant_1, name: "Sofa")
-    @item_2 = create(:item, user: @merchant_1, name: "Chair")
+RSpec.describe "adding items to their cart" do
+  context "as a visitor" do
+
+    before(:each) do
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, user: @merchant_1, name: "Sofa")
+      @item_2 = create(:item, user: @merchant_1, name: "Chair")
+    end
+
+    it "displays a message" do
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("You now have 1 #{@item_1.name} in your cart.")
+      expect(current_path).to eq(items_path)
+    end
+
+    it "the message correctly increments for multiple items" do
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      visit item_path(@item_2)
+      click_button "Add to Cart"
+
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("You now have 2 #{@item_1.name}s in your cart.")
+    end
+
+    it "displays the total number of items in the cart" do
+      visit item_path(@item_1)
+
+      expect(page).to have_content("Cart: 0")
+
+      click_button "Add to Cart"
+
+      expect(page).to have_content("Cart: 1")
+
+      visit item_path(@item_2)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("Cart: 2")
+
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("Cart: 3")
+    end
   end
 
-  it "displays a message" do
-    visit item_path(@item_1)
-    click_button "Add to Cart"
+  context "as a user" do
 
-    expect(page).to have_content("You now have 1 #{@item_1.name} in your cart.")
-  end
+    before(:each) do
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, user: @merchant_1, name: "Sofa")
+      @item_2 = create(:item, user: @merchant_1, name: "Chair")
+      @user_1 = create(:user)
 
-  it "the message correctly increments for multiple items" do
-    visit item_path(@item_1)
-    click_button "Add to Cart"
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+    end
 
-    visit item_path(@item_2)
-    click_button "Add to Cart"
+    it "displays a message" do
+      visit item_path(@item_1)
+      click_button "Add to Cart"
 
-    visit item_path(@item_1)
-    click_button "Add to Cart"
+      expect(page).to have_content("You now have 1 #{@item_1.name} in your cart.")
+    end
 
-    expect(page).to have_content("You now have 2 #{@item_1.name}s in your cart.")
-  end
+    it "the message correctly increments for multiple items" do
+      visit item_path(@item_1)
+      click_button "Add to Cart"
 
-  it "displays the total number of items in the cart" do
-    visit item_path(@item_1)
+      visit item_path(@item_2)
+      click_button "Add to Cart"
 
-    expect(page).to have_content("Cart: 0")
+      visit item_path(@item_1)
+      click_button "Add to Cart"
 
-    click_button "Add to Cart"
+      expect(page).to have_content("You now have 2 #{@item_1.name}s in your cart.")
+    end
 
-    expect(page).to have_content("Cart: 1")
+    it "displays the total number of items in the cart" do
+      visit item_path(@item_1)
 
-    visit item_path(@item_2)
-    click_button "Add to Cart"
+      expect(page).to have_content("Cart: 0")
 
-    expect(page).to have_content("Cart: 2")
+      click_button "Add to Cart"
 
-    visit item_path(@item_1)
-    click_button "Add to Cart"
+      expect(page).to have_content("Cart: 1")
 
-    expect(page).to have_content("Cart: 3")
+      visit item_path(@item_2)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("Cart: 2")
+
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      expect(page).to have_content("Cart: 3")
+    end
   end
 end
