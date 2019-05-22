a71ab1f579f61ac1fc817d3e9176f5763d24e156
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-22 17:19:06 -0600

    Completes User Story 16. All tests passing.
    Co-Authored-By: Billy Urrutia <CosmicSpagetti@users.noreply.github.com>
    Co-Authored-By: Michael Karnes <mikekarnes123@users.noreply.github.com>

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -5,11 +5,15 @@ RSpec.describe "As any kind of user, " do
     before :each do
       @merchant_1 = create(:merchant)
       @item_1 = create(:item, user: @merchant_1, inventory: 15)
+
+      @order_item_1 = create(:fulfilled_order_item, item: @item_1, created_at: 3.days.ago, updated_at: 1.days.ago)
+      @order_item_2 = create(:fulfilled_order_item, item: @item_1, created_at: 2.days.ago, updated_at: 1.days.ago)
+      @order_item_3 = create(:fulfilled_order_item, item: @item_1, created_at: 4.days.ago, updated_at: 1.days.ago)
+
+      visit item_path(@item_1)
     end
 
     it "I can see all the information for the item" do
-      visit item_path(@item_1)
-
       expect(current_path).to eq(item_path(@item_1))
 
       expect(page).to have_content(@item_1.name)
@@ -18,10 +22,7 @@ RSpec.describe "As any kind of user, " do
       expect(page).to have_content(@merchant_1.name)
       expect(page).to have_content(@item_1.inventory)
       expect(page).to have_content(@item_1.price)
-    end
-
-    xit "it can display average fulfillment time" do
-
+      expect(page).to have_content(@item_1.average_fulfillment_time)
     end
   end
 end
