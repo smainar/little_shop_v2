7cc363886c59c3f241fd55dd934bd31e15909e72
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-21 18:12:41 -0600

    Adds feature test for any user to logout. Test coverage at 100% Need to clear cart, when cart feature is completed.
    Co-Authored-By: Michael Karnes <mikekarnes123@users.noreply.github.com>
    Co-Authored-By: null <hsmitha26@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
 
   get '/profile', to: "users#show"
   get '/register', to: "users#new"
