f5bf419c3f7873135a7e64c7e86de9471f11d166
Michael Karnes <44683008+mikekarnes123@users.noreply.github.com>
2019-05-23 21:41:54 -0600

    Update merchant_index_spec.rb

--- spec/features/users/merchant_index_spec.rb
+++ spec/features/users/merchant_index_spec.rb
@@ -21,9 +21,9 @@ RSpec.describe "Merchant Index", type: :feature do
         expect(page).to have_content(active_merchant_2.state)
         expect(page).to have_content(Date.strptime(active_merchant_2.created_at.to_s))
       end
-        expect(page).to_not have_content(inactive_merchant.name)
-        expect(page).to_not have_content(inactive_merchant.city)
-        expect(page).to_not have_content(inactive_merchant.state)
+      expect(page).to_not have_content(inactive_merchant.name)
+      expect(page).to_not have_content(inactive_merchant.city)
+      expect(page).to_not have_content(inactive_merchant.state)
     end
   end
 end
