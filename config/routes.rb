17bf57275e017324e0a8f8fb246f956f198778cf
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-24 17:54:42 -0600

    Add comments to routes that should be under the user folder for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
 
-  get '/profile', to: "users#show"
-  patch '/profile', to: "users#update"
-  get '/profile/edit', to: "users#edit"
+  get '/profile', to: "users#show" # to-do: move to "user/users#show" using the profile scope / user module
+  patch '/profile', to: "users#update" # to-do: move to "user/users#update" using the profile scope / user module
+  get '/profile/edit', to: "users#edit" # to-do: move to "user/users#edit" using the profile scope / user module
 
   scope :profile, module: :user, as: :user do
     resources :orders, only: :index
