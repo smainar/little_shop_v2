91f4a014a35bb943e0e3a527491a96c4db66b51f
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-25 17:41:19 -0600

    Initial test setup for user story 30

---
+++ spec/features/profile/cancel_order_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "User cancels an order", type: :feature do
+  it "if order is pending there is a button to cancel" do
+
+  end
+
+  xit "if order is not pending there is no button to cancel" do
+
+  end
+end
