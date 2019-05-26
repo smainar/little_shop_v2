c4bda2acb632d2d3520790a8f356277ccdb402bb
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-26 17:48:41 -0600

    Add merchant/items#disable action

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -5,4 +5,17 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def new
   end
+
+  def disable
+    item = Item.find(params[:id])
+
+    if item.active && item.user == current_user
+      item.update(active: false)
+      flash[:success] = "#{item.name} is no longer for sale"
+    else
+      flash[:notice] = "#{item.name} is already disabled"
+    end
+
+    redirect_to merchant_items_path
+  end
 end
--- spec/features/merchant/items/enable_disable_spec.rb
+++ spec/features/merchant/items/enable_disable_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Enabling/Disabling Items" do
       end
 
       expect(current_path).to eq(merchant_items_path)
-      expect(page).to have_content("#{@active_item} is no longer for sale")
+      expect(page).to have_content("#{@active_item.name} is no longer for sale")
 
       within("#item-#{@active_item.id}") do
         expect(page).to have_button("Enable Item")
@@ -26,11 +26,34 @@ RSpec.describe "Enabling/Disabling Items" do
       end
     end
 
+    # to-do: figure out how to get `go_back` capybara method working to ensure full test coverage
     xit "inactive items can't be re-disabled" do
-      visit merchant_disable_item_path(@inactive_item)
+      visit merchant_items_path
+
+      within("#item-#{@active_item.id}") do
+        click_button "Disable Item"
+      end
+
+      # Hit the back button in the browser:
+      go_back
+      # The Disable Item button should be there again (even though the item is disabled in the database)
+
+      within("#item-#{@active_item.id}") do
+        click_button "Disable Item"
+      end
 
       expect(current_path).to eq(merchant_items_path)
-      expect(page).to have_content("#{@inactive_item} is already disabled")
+      expect(page).to have_content("#{@inactive_item.name} is already disabled")
+    end
+
+    xit "other merchant's items can't be disabled" do
+      other_merchant_item = create(:item)
+
+      # to-do: figure out how to effectively test (we want to PATCH, not GET), or delete this test
+      visit merchant_disable_item_path(other_merchant_item)
+
+      expect(current_path).to eq(merchant_items_path)
+      expect(page).to have_content("You don't have permission to delete item #{other_merchant_item.id}")
     end
 
     xit "has a button to enable an inactive item" do
@@ -41,7 +64,7 @@ RSpec.describe "Enabling/Disabling Items" do
       end
 
       expect(current_path).to eq(merchant_items_path)
-      expect(page).to have_content("#{@inactive_item} is now available for sale")
+      expect(page).to have_content("#{@inactive_item.name} is now available for sale")
 
       within("#item-#{@inactive_item.id}") do
         expect(page).to_not have_button("Enable Item")
@@ -53,7 +76,7 @@ RSpec.describe "Enabling/Disabling Items" do
       visit merchant_enable_item_path(@active_item)
 
       expect(current_path).to eq(merchant_items_path)
-      expect(page).to have_content("#{@active_item} is already active")
+      expect(page).to have_content("#{@active_item.name} is already active")
     end
   end
 end
