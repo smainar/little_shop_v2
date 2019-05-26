64e889cfa35656d4a87ae09fa2e6daa924750d76
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-26 13:44:29 -0600

    Add User::inactive_merchants method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,14 +21,26 @@ RSpec.describe User, type: :model do
   end
 
   describe 'Class Methods' do
-    describe '.active_merchants' do
+    describe '::active_merchants' do
       it 'should return all active merchants' do
         active_merchant_1 = create(:merchant)
         active_merchant_2 = create(:merchant)
         inactive_merchant = create(:inactive_merchant)
         regular_active_user = create(:user)
+        active_admin = create(:admin)
 
         expect(User.active_merchants).to eq([active_merchant_1, active_merchant_2])
+      end
+    end
+
+    describe '::inactive_merchants' do
+      it 'should return all inactive (disabled) merchants' do
+        active_merchant = create(:merchant)
+        inactive_merchant_1 = create(:inactive_merchant)
+        inactive_merchant_2 = create(:inactive_merchant)
+        inactive_user = create(:inactive_user)
+
+        expect(User.inactive_merchants).to eq([inactive_merchant_1, inactive_merchant_2])
       end
     end
   end
