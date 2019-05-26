d37c424a92325add8d205c095eadac68ce27099d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-26 09:21:22 -0600

    Add tests for merchant items index contents (different types of items)

--- spec/features/merchants/dashboard/items_index_spec.rb
+++ spec/features/merchants/dashboard/items_index_spec.rb
@@ -4,7 +4,17 @@ RSpec.describe "Merchant Items Index", type: :feature do
   context "as a merchant" do
     before(:each) do
       @merchant = create(:merchant)
-      @item_1 , @item_2 , @item_3 , @item_4 = create_list(:item, 4, user: @merchant)
+
+      @other_merchant_item = create(:item)
+
+      @active_never_ordered_item = create(:item, user: @merchant)
+      @disabled_never_ordered_item = create(:inactive_item, user: @merchant)
+      @active_ordered_item = create(:item, user: @merchant)
+      @disabled_ordered_item = create(:inactive_item, user: @merchant)
+
+      @order = create(:order)
+      @oi_1 = create(:order_item, order: @order, item: @active_ordered_item)
+      @oi_2 = create(:order_item, order: @order, item: @disabled_ordered_item)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
     end
@@ -18,7 +28,58 @@ RSpec.describe "Merchant Items Index", type: :feature do
     end
 
     it "has info for all the items I'm selling" do
+      visit merchant_items_path
 
+      expect(page).to have_link("Add a New Item")
+      expect(page).to_not have_content(@other_merchant_item.id)
+
+      within("#item-#{@active_never_ordered_item.id}") do
+        expect(page).to have_link(@active_never_ordered_item.name)
+        expect(page).to have_content("ID: #{@active_never_ordered_item.id}")
+        expect(page).to have_css("img[src*='#{@active_never_ordered_item.image}']")
+        expect(page).to have_content(number_to_currency(@active_never_ordered_item.price))
+        expect(page).to have_content("#{@active_never_ordered_item.inventory} in stock")
+        expect(page).to have_link("Edit Item")
+        expect(page).to_not have_link("Enable Item")
+        expect(page).to have_link("Disable Item")
+        expect(page).to have_link("Delete Item")
+      end
+
+      within("#item-#{@disabled_never_ordered_item.id}") do
+        expect(page).to have_link(@disabled_never_ordered_item.name)
+        expect(page).to have_content("ID: #{@disabled_never_ordered_item.id}")
+        expect(page).to have_css("img[src*='#{@disabled_never_ordered_item.image}']")
+        expect(page).to have_content(number_to_currency(@disabled_never_ordered_item.price))
+        expect(page).to have_content("#{@disabled_never_ordered_item.inventory} in stock")
+        expect(page).to have_link("Edit Item")
+        expect(page).to have_link("Enable Item")
+        expect(page).to_not have_link("Disable Item")
+        expect(page).to have_link("Delete Item")
+      end
+
+      within("#item-#{@active_ordered_item.id}") do
+        expect(page).to have_link(@active_ordered_item.name)
+        expect(page).to have_content("ID: #{@active_ordered_item.id}")
+        expect(page).to have_css("img[src*='#{@active_ordered_item.image}']")
+        expect(page).to have_content(number_to_currency(@active_ordered_item.price))
+        expect(page).to have_content("#{@active_ordered_item.inventory} in stock")
+        expect(page).to have_link("Edit Item")
+        expect(page).to_not have_link("Enable Item")
+        expect(page).to have_link("Disable Item")
+        expect(page).to_not have_link("Delete Item")
+      end
+
+      within("#item-#{@disabled_never_ordered_item.id}") do
+        expect(page).to have_link(@disabled_never_ordered_item.name)
+        expect(page).to have_content("ID: #{@disabled_never_ordered_item.id}")
+        expect(page).to have_css("img[src*='#{@disabled_never_ordered_item.image}']")
+        expect(page).to have_content(number_to_currency(@disabled_never_ordered_item.price))
+        expect(page).to have_content("#{@disabled_never_ordered_item.inventory} in stock")
+        expect(page).to have_link("Edit Item")
+        expect(page).to have_link("Enable Item")
+        expect(page).to_not have_link("Disable Item")
+        expect(page).to_not have_link("Delete Item")
+      end
     end
   end
 end
