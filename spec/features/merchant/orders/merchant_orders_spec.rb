14872af6b5933c1d2bba99c46584b588d9c985e4
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-27 16:38:28 -0600

    moved feature test intor merchant/orders file.

--- spec/features/merchant/orders/merchant_orders_spec.rb
+++ spec/features/merchant/orders/merchant_orders_spec.rb
@@ -58,5 +58,19 @@ RSpec.describe 'As a merchant: ' do
 
       expect(page).to_not have_content(@order_3.id)
     end
+    # As a merchant
+    # When I visit an order show page from my dashboard
+    # I see the customer's name and address
+    # I only the items in the order that are being purchased from my inventory
+    # I do not see any items in the order being purchased from other merchants
+    # For each item, I see the following information:
+    # - the name of the item, which is a link to my item's show page
+    # - a small thumbnail of the item
+    # - my price for the item
+    # - the quantity the user wants to purchase
+    it "I am taken to order show page from my dashboard" do
+      click_on "Order #{@order_1.id}"
+      expect(current_path).to eq(dashboard_order_path(@order))
+    end
   end
 end
