e5e0a20690b1ff51d863159b6045af724b869e40
Stella Mainar <mainars09@alumni.hanover.edu>
2019-05-27 07:48:23 -0600

    Add feature test to display flash message when merchant's account is disabled

--- spec/features/merchants/merchant_index_spec.rb
+++ spec/features/merchants/merchant_index_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe "Merchant Index", type: :feature do
     end
   end
 
-  context "as an admin" do
+  context "as an admin user" do
     before(:each) do
       @admin = User.create!(email:    "abc@def.com",
                             password: "pw123",
@@ -85,6 +85,21 @@ RSpec.describe "Merchant Index", type: :feature do
       within("#merchant-id-#{@disabled_merchant.id}") do
         expect(page).to_not have_button("Disable Merchant")
         expect(page).to have_button("Enable Merchant")
+      end
+    end
+
+    context "as an admin merchant" do
+      it "when I click on a 'Disable Merchant' button, it displays a flash message that the merchant's account is now disabled" do
+        visit admin_merchants_path
+
+        within("#merchant-id-#{@active_merchant.id}") do
+          expect(page).to have_button("Disable Merchant")
+        end
+
+        click_button "Disable Merchant"
+
+        expect(current_path).to eq(admin_merchants_path)
+        expect(page).to have_content("#{@active_merchant.name}'s account is now disabled.")
       end
     end
   end
