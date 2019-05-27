b38c7a0295f22746d6a6fa53a8c9634889d5f5e6
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-27 14:22:14 -0600

    removes unwanted comments from test file

--- spec/features/merchants/dashboard/merchant_orders_spec.rb
+++ spec/features/merchants/dashboard/merchant_orders_spec.rb
@@ -1,14 +1,6 @@
 require "rails_helper"
 include ActionView::Helpers::TextHelper
-# As a merchant
-# When I visit my dashboard ("/dashboard")
-# If any users have pending orders containing items I sell
-# Then I see a list of these orders.
-# Each order listed includes the following information:
-# - the ID of the order, which is a link to the order show page ("/dashboard/orders/15")
-# - the date the order was made
-# - the total quantity of my items in the order
-# - the total value of my items for that order
+
 RSpec.describe 'As a merchant: ' do
   describe "when I visit my dashboard, " do
     before :each do
