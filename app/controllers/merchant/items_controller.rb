54312cda78e0e6cd4a5ae3e8bc3fd2b68b3759a6
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-28 17:31:12 -0600

    Add merchant/items#create action

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,7 +4,18 @@ class Merchant::ItemsController < Merchant::BaseController
   end
 
   def new
-    @item = Item.new
+    @item = Item.new(user: current_user)
+  end
+
+  def create
+    @item = current_user.items.new(item_params)
+    if @item.save!
+      flash[:success] = "#{@item.name} has been added"
+      redirect_to merchant_items_path
+    else
+      flash[:error] = @item.errors.full_messages.join(". ")
+      render :new
+    end
   end
 
   def disable
@@ -27,5 +38,11 @@ class Merchant::ItemsController < Merchant::BaseController
     end
 
     redirect_to merchant_items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :price, :inventory)
   end
 end
