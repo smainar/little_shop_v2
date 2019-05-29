81861d377204eaf406c73463ccd77917c2e81c9c
mikekarnes123 <michaelkarnes.mk@gmail.com>
2019-05-29 13:57:37 -0600

    adds sorting

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,5 +1,8 @@
 class Admin::AdminsController < Admin::BaseController
   def show
-    @orders = Order.all
+    @packaged_orders = Order.packaged_orders
+    @pending_orders = Order.pending_orders
+    @shipped_orders = Order.shipped_orders
+    @cancelled_orders = Order.cancelled_orders
   end
 end
