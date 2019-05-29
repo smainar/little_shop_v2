62ba1e22eaaf68ad90e193166fe0a084a790fc6a
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-29 12:52:51 -0600

    Add feature tests for editing all fields of a merchant's items

--- spec/features/merchant/items/edit_spec.rb
+++ spec/features/merchant/items/edit_spec.rb
@@ -64,10 +64,6 @@ RSpec.describe "Merchant Edits an Item", type: :feature do
       new_name = "Big Couch"
 
       fill_in "item[name]", with: new_name
-      # fill_in "item[description]", with: "It's a very large couch"
-      # fill_in "item[image]", with: "https://cdn.sofadreams.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/e/megasofa_leder_wohnlandschaft_big_couch_concept_beleuchtung_schwarz_1_1.jpg"
-      # fill_in "item[price]", with: "50.00"
-      # fill_in "item[inventory]", with: "75"
 
       click_on "Update Item"
 
@@ -85,7 +81,85 @@ RSpec.describe "Merchant Edits an Item", type: :feature do
       end
     end
 
-    xit "more tests for editing other fields"
+    it "I can edit an item's description by filling out a form" do
+      visit edit_merchant_item_path(@item)
+
+      expect(current_path).to eq("/dashboard/items/#{@item.id}/edit")
+
+      new_description = "It's a very large couch"
+
+      fill_in "item[description]", with: new_description
+
+      click_on "Update Item"
+
+      expect(current_path).to eq(merchant_items_path)
+
+      expect(page).to have_content("#{@item.reload.name} has been updated")
+      expect(@item.reload.description).to eq(new_description)
+    end
+
+    it "I can edit an item's image by filling out a form" do
+      visit edit_merchant_item_path(@item)
+
+      expect(current_path).to eq("/dashboard/items/#{@item.id}/edit")
+
+      new_image = "https://slimages.macysassets.com/is/image/MCY/products/0/optimized/8235120_fpx.tif?op_sharpen=1&wid=500&hei=613&fit=fit,1&$filtersm$"
+
+      fill_in "item[image]", with: new_image
+
+      click_on "Update Item"
+
+      expect(current_path).to eq(merchant_items_path)
+
+      expect(page).to have_content("#{@item.reload.name} has been updated")
+      expect(@item.reload.image).to eq(new_image)
+
+      within("#item-#{@item.id}") do
+        expect(page).to have_css("img[src*='#{new_image}']")
+      end
+    end
+
+    it "I can edit an item's price by filling out a form" do
+      visit edit_merchant_item_path(@item)
+
+      expect(current_path).to eq("/dashboard/items/#{@item.id}/edit")
+
+      new_price = "3.23"
+
+      fill_in "item[price]", with: new_price
+
+      click_on "Update Item"
+
+      expect(current_path).to eq(merchant_items_path)
+
+      expect(page).to have_content("#{@item.reload.name} has been updated")
+      expect(@item.reload.price.to_f).to eq(new_price.to_f)
+
+      within("#item-#{@item.id}") do
+        expect(page).to have_content(number_to_currency(new_price))
+      end
+    end
+
+    it "I can edit an item's inventory by filling out a form" do
+      visit edit_merchant_item_path(@item)
+
+      expect(current_path).to eq("/dashboard/items/#{@item.id}/edit")
+
+      new_inventory = 42
+
+      fill_in "item[inventory]", with: new_inventory
+
+      click_on "Update Item"
+
+      expect(current_path).to eq(merchant_items_path)
+
+      expect(page).to have_content("#{@item.reload.name} has been updated")
+      expect(@item.reload.inventory).to eq(new_inventory)
+
+      within("#item-#{@item.id}") do
+        expect(page).to have_content("#{new_inventory} in stock")
+      end
+    end
 
     xit "I cannot leave the name field blank" do
       visit new_merchant_item_path
