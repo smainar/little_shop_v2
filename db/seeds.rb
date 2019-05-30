af113835d1df58706ae67d735df6e468bd940372
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-05-29 18:35:23 -0600

    added missing seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,34 +29,34 @@ create_list(:item, 10, user: merchant_1)
 
 inactive_item_1 = create(:inactive_item, user: merchant_1)
 inactive_item_2 = create(:inactive_item, user: inactive_merchant_1)
-#
-# Random.new_seed
-# rng = Random.new
-#
-# # pending order, none fulfilled
-# order = create(:order, user: user)
-# create(:order_item, order: order, item: item_1, price_per_item: 1, quantity: 1)
-# create(:order_item, order: order, item: item_2, price_per_item: 2, quantity: 1)
-#
-# # pending order, partially fulfilled
-# order = create(:order, user: user)
-# create(:order_item, order: order, item: item_1, price_per_item: 1, quantity: 1)
-# create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).days.ago, updated_at: rng.rand(23).hours.ago)
-#
-# # packaged order
-# order = create(:packaged_order, user: user)
-# create(:fulfilled_order_item, order: order, item: item_1, price_per_item: 1, quantity: 1, created_at: (rng.rand(3)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-# create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-#
-# # shipped order, cannot be cancelled
-# order = create(:shipped_order, user: user)
-# create(:fulfilled_order_item, order: order, item: item_1, price_per_item: 1, quantity: 1, created_at: (rng.rand(3)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
-# create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-#
-# # cancelled order
-# order = create(:cancelled_order, user: user)
-# create(:order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
-#create(:order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+Random.new_seed
+rng = Random.new
+
+# pending order, none fulfilled
+order = create(:order, user: user)
+create(:order_item, order: order, item: item_1, price_per_item: 1, quantity: 1)
+create(:order_item, order: order, item: item_2, price_per_item: 2, quantity: 1)
+
+# pending order, partially fulfilled
+order = create(:order, user: user)
+create(:order_item, order: order, item: item_1, price_per_item: 1, quantity: 1)
+create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).days.ago, updated_at: rng.rand(23).hours.ago)
+
+# packaged order
+order = create(:packaged_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price_per_item: 1, quantity: 1, created_at: (rng.rand(3)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+# shipped order, cannot be cancelled
+order = create(:shipped_order, user: user)
+create(:fulfilled_order_item, order: order, item: item_1, price_per_item: 1, quantity: 1, created_at: (rng.rand(3)+1).days.ago, updated_at: rng.rand(59).minutes.ago)
+create(:fulfilled_order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
+
+# cancelled order
+order = create(:cancelled_order, user: user)
+create(:order_item, order: order, item: item_2, price_per_item: 2, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
+create(:order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: (rng.rand(23)+1).hour.ago, updated_at: rng.rand(59).minutes.ago)
 
 
 puts 'seed data finished'
