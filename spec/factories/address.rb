2c2c08b03cf0d04b6986cbf4efe5cc37c7b09429
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-02 11:06:38 -0600

    Add factory for Address

---
+++ spec/factories/address.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :address, class: Address do
+    association :user, factory: :user
+    sequence(:street) { |n| "Street #{n}" }
+    sequence(:city) { |n| "City #{n}" }
+    sequence(:state) { |n| "State #{n}" }
+    sequence(:zip) { |n| "Zip #{n}" }
+    role { 0 }
+    active { true }
+  end
+end
