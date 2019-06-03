11e2588bb50d736a31f605c1c9a0a49d46ec3e26
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-03 17:13:51 -0600

    Migration to add nickname column to Addresses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190602061200) do
+ActiveRecord::Schema.define(version: 20190603222641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20190602061200) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", default: "home"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
