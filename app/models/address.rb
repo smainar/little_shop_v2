0ba4e14fb7c09ed60e1925ba72ea3d45d86e990d
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-04 20:01:24 -0600

    Add 'has_many :orders' relationship to Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user
-  # has_many :orders
+  has_many :orders
+  
   validates_presence_of :street,
                         :city,
                         :state,
